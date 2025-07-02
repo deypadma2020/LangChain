21a5da913ad185bd93b474cc3a08f7a173ece677
deypadma2020 <deypadma2020@gmail.com>
2025-07-02 23:06:17 +0530

    Create a structured output using TypedDict Schema

--- WithStructuredOutput/with_structured_output_typeddict.py
+++ WithStructuredOutput/with_structured_output_typeddict.py
@@ -15,6 +15,7 @@ class Review(TypedDict):
     name: Annotated[Optional[str], "Write the name of the reviewer"]
 
 structured_model = model.with_structured_output(Review)
+
 response = structured_model.invoke(
     """
 I recently upgraded to the Samsung Galaxy S24 Ultra, and I must say, it’s an absolute powerhouse! The Snapdragon 8 Gen 3 processor makes everything lightning fast—whether I’m gaming, multitasking, or editing photos. The 5000mAh battery easily lasts a full day even with heavy use, and the 45W fast charging is a lifesaver.
