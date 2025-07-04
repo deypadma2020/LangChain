01a948396c2f76ab6bcb77fe2f5adeaf7c25d23b
deypadma2020 <deypadma2020@gmail.com>
2025-07-05 00:43:47 +0530

    add pydantic output parser

---
+++ OutputParser/PydanticOutputParser_Anthropic.py
@@ -0,0 +1,29 @@
+from langchain_anthropic import ChatAnthropic
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+load_dotenv()
+
+model = ChatAnthropic(model="claude-3-5-sonnet-20240620")
+
+class Person(BaseModel):
+    name: str = Field(description="Name of the Person")
+    age: int = Field(gt=18, description="Age of the person")
+    city: str = Field(description="Name of the city the person belongs to")
+
+parser = PydanticOutputParser(pydantic_object=Person)
+
+template = PromptTemplate(
+    template="Generate the name, age and city of a fictional {place} person \n {format_instruction}",
+    input_variables=['place'],
+    partial_variables={
+        'format_instruction' : parser.get_format_instructions()
+    }
+)
+
+chain = template | model | parser
+
+response = chain.invoke({"place": 'Sweden'})
+
+print(response)
