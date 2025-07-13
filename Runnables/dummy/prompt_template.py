2aee60afcd3fda01c4a815923eae68adf175a175
deypadma2020 <deypadma2020@gmail.com>
2025-07-13 21:12:34 +0530

    dummy prompt template class

---
+++ Runnables/dummy/prompt_template.py
@@ -0,0 +1,8 @@
+class DemoPromptTemplate:
+    def __init__(self, template, input_variables):
+        self.template = template
+        self.input_variables = input_variables
+
+    def format(self, input_dict):
+        return self.template.format(**input_dict)
+    
