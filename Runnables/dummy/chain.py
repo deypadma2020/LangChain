f6bad3538a0022e9bae5cae7af3cebe2099a0421
deypadma2020 <deypadma2020@gmail.com>
2025-07-13 21:14:33 +0530

    dummy chain class

---
+++ Runnables/dummy/chain.py
@@ -0,0 +1,10 @@
+class DemoLLMChain():
+    def __init__(self, llm, prompt):
+        self.llm = llm
+        self.prompt = prompt
+
+    def run(self, input_dict):
+        final_prompt = self.prompt.format(input_dict)
+        result = self.llm.predict(final_prompt)
+
+        return result['response']
