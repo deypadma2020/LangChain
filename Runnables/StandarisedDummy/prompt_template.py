e6e95799bdc855672d781cd98eacb1ceabe71857
deypadma2020 <deypadma2020@gmail.com>
2025-07-13 21:19:33 +0530

    standarise dummy prompt template class with invoke function

---
+++ Runnables/StandarisedDummy/prompt_template.py
@@ -0,0 +1,13 @@
+from runnable_class import Runnable
+
+class DemoPromptTemplate(Runnable):
+    def __init__(self, template, input_variables):
+        self.template = template
+        self.input_variables = input_variables
+
+    def invoke(self, input_dict):
+        return self.template.format(**input_dict)
+
+    def format(self, input_dict):
+        return self.template.format(**input_dict)
+    
