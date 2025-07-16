c6a1e58a67ace20be04a445690626cc8e5befcb6
deypadma2020 <deypadma2020@gmail.com>
2025-07-16 10:44:03 +0530

    add a different way of use runnable lambda

--- Runnables/runnable_premitives/runnable_lambda.py
+++ Runnables/runnable_premitives/runnable_lambda.py
@@ -1,5 +1,4 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_anthropic import ChatAnthropic
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain.schema.runnable import RunnableSequence, RunnableParallel, RunnablePassthrough, RunnableLambda
@@ -16,10 +15,17 @@ model = ChatGoogleGenerativeAI(model="gemini-2.0-flash-001")
 
 parser = StrOutputParser()
 
+joke_generation_chain = RunnableSequence(prompt, model, parser)
+
+# parallel_chain = RunnableParallel(
+#     {
+#         'joke': RunnablePassthrough(),
+#         'word_count': RunnableLambda(lambda x: len(x.split()))
+#     }
+# )
+
 def word_count(text):
     return len(text.split())
-
-joke_generation_chain = RunnableSequence(prompt, model, parser)
 
 parallel_chain = RunnableParallel(
     {
