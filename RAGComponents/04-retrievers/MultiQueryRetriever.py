276c6dc871eb5d712e18ddf1acbcc1e1f1db518c
deypadma2020 <deypadma2020@gmail.com>
2025-07-21 00:56:33 +0530

    create a Multi Query Retriever - to remove query ambiguity and give better response

--- RAGComponents/04-retrievers/MultiQueryRetriever.py
+++ RAGComponents/04-retrievers/MultiQueryRetriever.py
@@ -1,17 +1,23 @@
+from langchain_anthropic import ChatAnthropic
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEndpointEmbeddings
 from langchain_core.documents import Document
+from langchain.retrievers.multi_query import MultiQueryRetriever
 from dotenv import load_dotenv
 
 load_dotenv()
 
 docs = [
-    Document(page_content="LangChain makes it easy to work with LLMs."),
-    Document(page_content="LangChain is used to build LLM based applications."),
-    Document(page_content="Chroma is used to store and search document embeddings."),
-    Document(page_content="Embeddings are vector representations of text."),
-    Document(page_content="MMR helps you get diverse results when doing similarity search."),
-    Document(page_content="LangChain supports Chroma, FAISS, Pinecone, and more."),
+    Document(page_content="Regular walking boosts heart health and can reduce symptoms of depression.", metadata={"source": "H1"}),
+    Document(page_content="Consuming leafy greens and fruits helps detox the body and improve longevity.", metadata={"source": "H2"}),
+    Document(page_content="Deep sleep is crucial for cellular repair and emotional regulation.", metadata={"source": "H3"}),
+    Document(page_content="Mindfulness and controlled breathing lower cortisol and improve mental clarity.", metadata={"source": "H4"}),
+    Document(page_content="Drinking sufficient water throughout the day helps maintain metabolism and energy.", metadata={"source": "H5"}),
+    Document(page_content="The solar energy system in modern homes helps balance electricity demand.", metadata={"source": "I1"}),
+    Document(page_content="Python balances readability with power, making it a popular system design language.", metadata={"source": "I2"}),
+    Document(page_content="Photosynthesis enables plants to produce energy by converting sunlight.", metadata={"source": "I3"}),
+    Document(page_content="The 2022 FIFA World Cup was held in Qatar and drew global energy and excitement.", metadata={"source": "I4"}),
+    Document(page_content="Black holes bend spacetime and store immense gravitational energy.", metadata={"source": "I5"}),
 ]
 
 embedding_model = HuggingFaceEndpointEmbeddings(
@@ -26,17 +32,32 @@ vectorstore = FAISS.from_documents(
 # Save to disk if needed
 vectorstore.save_local("RAGComponents/04-retrievers/vectorstore/faiss_store")
 
-retriever = vectorstore.as_retriever(
-    search_type="mmr",
-    search_kwargs={
-        "k": 3,
-        "lambda_mult": 0.5 # "lambda_mult": 1 -> behave like as same as regular retriver
-    }
+# vectorstore = FAISS.load_local(
+#     folder_path="RAGComponents/04-retrievers/vectorstore",
+#     embeddings=embedding_model
+# )
+
+similarity_retriever = vectorstore.as_retriever(
+    search_type="similarity",
+    search_kwargs={"k": 5}
 )
 
-query = "What is langchain?"
-response_docs = retriever.invoke(query)
+multiquery_retriever = MultiQueryRetriever.from_llm(
+    retriever=vectorstore.as_retriever(search_kwargs={"k": 5}),
+    llm=ChatAnthropic(model="claude-3-5-sonnet-20240620"),
+)
 
-for i, doc in enumerate(response_docs):
-    print(f"\n--- Response {i+1} ---")
-    print(f"Content:\n{doc.page_content}...")
+query = "How to improve energy levels and maintain balance?"
+
+similarity_response = similarity_retriever.invoke(query)
+multiquery_response = multiquery_retriever.invoke(query)
+
+for i, doc in enumerate(similarity_response):
+    print(f"\n--- Result {i+1} ---")
+    print(doc.page_content)
+
+print("*"*150)
+
+for i, doc in enumerate(multiquery_response):
+    print(f"\n--- Result {i+1} ---")
+    print(doc.page_content)
