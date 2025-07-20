92fa4a06a1a75b50ede294efcfffc2beb3d86d95
deypadma2020 <deypadma2020@gmail.com>
2025-07-21 00:07:02 +0530

    create a wikipedia retriever to get generate response

---
+++ RAGComponents/04-retrievers/WikipediaRetriever.py
@@ -0,0 +1,12 @@
+from langchain_community.retrievers import WikipediaRetriever
+
+retriever = WikipediaRetriever(top_k_results=2, lang="en")
+
+query = "the geopolitical history of india and pakistan from the perspective of a chinese"
+
+response_docs = retriever.invoke(query)
+
+for i, doc in enumerate(response_docs):
+    print(f"\n--- Response {i+1} ---")
+    print(f"Content:\n{doc.metadata}...")
+    print(f"Content:\n{doc.page_content}...")
